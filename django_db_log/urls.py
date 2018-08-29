0ea353928b31a0d2e5ec2a231548ff656e768c4b
Eduard Erja <eduard.erja@gmail.com>
2018-08-29 15:08:44 +0300

    Logging prototype

--- django_db_log/urls.py
+++ django_db_log/urls.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+
 from django.views.generic import TemplateView
+from django.contrib import admin
+from django.conf.urls import include, url
 
 from . import views
 
-
 app_name = 'django_db_log'
 urlpatterns = [
-    url(r'', TemplateView.as_view(template_name="base.html")),
-    ]
+    # url(r'', TemplateView.as_view(template_name="base.html")),
+    url(r'^admin/', include(admin.site.urls)),
+]
