4ce17fb0dbef52304cd1efc6d9ce0f71d4aa57e3
Eduard Erja <eduard.erja@gmail.com>
2018-08-31 14:21:26 +0300

    Solved problem in urls

--- django_db_log/urls.py
+++ django_db_log/urls.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 app_name = 'django_db_log'
 urlpatterns = [
-    # url(r'', ErrorListView.as_view(), name='error_list'),
-    # url(r'', TemplateView.as_view(template_name="base.html")),
-    url(r'admin/', include(admin.site.urls)),
+    url(r'admin/', admin.site.urls),
 ]
